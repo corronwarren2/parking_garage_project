fba30b824619bc1d251a7395596257b59ed72f24
corronwarren2 <86121210+corronwarren2@users.noreply.github.com>
2021-07-10 14:05:56 -0400

    parking lot python and README files uploaded

---
+++ parking_lot.py
@@ -0,0 +1,65 @@
+# classes:
+# parking garage
+# methods:
+# pay for parking
+# take ticket
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#  
+class ParkingGarage():
+    def __init__(self):
+        self.spaces = 0
+        self.available = 0
+        self.occupied = 0
+        pass
+
+    def newParkingLot(self, capacity):
+        self.spots = [-1] * capacity
+        self.capacity = capacity
+    return self.capacity
+        pass
+    
+    def emptySpaces():    
+        for i in range(len(self.spots))
+            if self.spots[i] == -1
+                return i
+        pass
+
+    return
+    
+    def takeTicket():
+        pass
+    return
+
+    def payForParking():
+        pass
+    return
+
+    def leaveGarage():
+        pass
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
